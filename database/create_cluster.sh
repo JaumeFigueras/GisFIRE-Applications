3133f99a9069604978d00f7cd9e1a9e297a7e0b5
JaumeFigueras <jaume.figueras@upc.edu>
2021-10-14 20:03:34 +0200

    Added PostGIS extension

--- database/create_cluster.sh
+++ database/create_cluster.sh
@@ -12,3 +12,4 @@ POSTGRESQL_VERSION=$4
 pg_createcluster -d "$FOLDER"/"$CLUSTER_NAME" -l "$FOLDER"/"$CLUSTER_NAME"/"$CLUSTER_NAME".log -p "$PORT" --start --start-conf auto "$POSTGRESQL_VERSION" "$CLUSTER_NAME"
 sudo -u postgres -- createuser -p "$PORT" -d -S -P -r "$CLUSTER_NAME"user
 sudo -u postgres -- createdb -p "$PORT" -E UTF8 -O "$CLUSTER_NAME"user "$CLUSTER_NAME"db
+sudo -u postgres -- psql -p "$PORT" -d "$CLUSTER_NAME"db -c "CREATE EXTENSION postgis;"
